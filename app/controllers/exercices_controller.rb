d33c666078069cabbdac9515578eeec08eb00386
Guibril <guibril.ndoye@gmail.com>
2015-02-19 18:12:24 +0100

    Thir commit

--- app/controllers/exercices_controller.rb
+++ app/controllers/exercices_controller.rb
@@ -3,12 +3,41 @@ class ExercicesController < ApplicationController
   def index
   end
 
+  def show
+    @exercice = Exercice.find(params[:id])
+  end
+
   def new
-    
+    @exercice = Exercice.new
+  end
+
+  def edit
+  @exercice = Exercice.find(params[:id])
   end
 
   def create
-    render plain: params[:exercices].inspect
+  @exercice = Exercice.new(exercice_params)
+
+    if @exercice.save
+      redirect_to @exercice
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @exercice = Exercice.find(params[:id])
+
+    if @exercice.update(exercice_params)
+      redirect_to @exercice
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def exercice_params
+    params.require(:exercices).permit(:text)
   end
 
 end
---
+++ db/migrate/20150219152943_change_text_to_exercices.rb
@@ -0,0 +1,4 @@
+class ChangeTextToExercices < ActiveRecord::Migration
+  def change
+  end
+end
